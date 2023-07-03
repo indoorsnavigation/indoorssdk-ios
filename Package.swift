b5d902c8b3d88af640f730537a434bc77d8dfa44
Резвых Тимофей <63452409+zabarik21@users.noreply.github.com>
2023-07-03 14:23:04 +0500

    v3.5.7

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 					name: "IndoorsSDK",
-					url: "https://core.indoorsnavi.pro/artifactory/ios/pro.indoorsnavi/indoorssdk/indoorssdk-3.5.6.zip",
-					checksum: "5f198232c3261b730a468a3c2ef086f7cd63632607b4ff9503c6926805360d5c"
+					url: "https://core.indoorsnavi.pro/artifactory/ios/pro.indoorsnavi/indoorssdk/indoorssdk-3.5.7.zip",
+					checksum: "ios/pro.indoorsnavi/indoorssdk/indoorssdk-3.5.7.zip"
 				)
 	]
 )
